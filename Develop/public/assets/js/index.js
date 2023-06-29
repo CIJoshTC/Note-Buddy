331a9c6c13cdaab9edc8955abfa5452ac947f720
Joshua <23joshtc@gmail.com>
2023-06-29 15:33:01 -0400

    updated index. js fetch api

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,46 @@
+// let noteTitle;
+// let noteText;
+// let saveNoteBtn;
+// let newNoteBtn;
+// let noteList;
+
+// if (window.location.pathname === '/notes') {
+//   noteTitle = document.querySelector('.note-title');
+//   noteText = document.querySelector('.note-textarea');
+//   saveNoteBtn = document.querySelector('.save-note');
+//   newNoteBtn = document.querySelector('.new-note');
+//   noteList = document.querySelectorAll('.list-container .list-group');
+// }
+
+// // Show an element
+// const show = (elem) => {
+//   elem.style.display = 'inline';
+// };
+
+// // Hide an element
+// const hide = (elem) => {
+//   elem.style.display = 'none';
+// };
+
+// // activeNote is used to keep track of the note in the textarea
+// let activeNote = {};
+
+// const getNotes = () =>
+//   fetch('/api/notes', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+// const saveNote = (note) =>
+//   fetch('/api/notes', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(note),
+//   });
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -9,38 +52,35 @@ if (window.location.pathname === '/notes') {
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
+  noteList = document.querySelector('.list-group');
 }
 
-// Show an element
 const show = (elem) => {
   elem.style.display = 'inline';
 };
 
-// Hide an element
 const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () =>
+const getNotes = () => 
   fetch('/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-    },
-  });
+     },
+   });
 
 const saveNote = (note) =>
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+     fetch('/api/notes', {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(note),
-  });
+     body: JSON.stringify(note),
+   });
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -45,3 +45,11 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
   });
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+
